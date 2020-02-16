bf596130636587899782788d212f24a66f5aae08
vladtf <vendeta0263@gmail.com>
2020-02-16 22:13:52 +0200

    Finished task 8 Generating a proper menu for budget and maxKcal set.

--- Olimpiada2016Judet/Forms/Optiuni.Designer.cs
+++ Olimpiada2016Judet/Forms/Optiuni.Designer.cs
@@ -50,10 +50,18 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -269,6 +277,12 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.textBox8);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.textBox10);
+            this.tabPage3.Controls.Add(this.label10);
+            this.tabPage3.Controls.Add(this.dataGridView2);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -287,6 +301,64 @@
             this.tabPage4.Text = "Grafic KCal";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(127, 469);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(240, 26);
+            this.textBox8.TabIndex = 40;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(48, 472);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 20);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Budget";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.Location = new System.Drawing.Point(127, 426);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(240, 26);
+            this.textBox10.TabIndex = 36;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(13, 432);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(108, 20);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "Necesar Zilnic";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 28);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(661, 342);
+            this.dataGridView2.TabIndex = 34;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(413, 423);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 85);
+            this.button3.TabIndex = 41;
+            this.button3.Text = "Genereaza";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Optiuni
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -301,6 +373,9 @@
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -329,5 +404,11 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- Olimpiada2016Judet/Forms/Optiuni.cs
+++ Olimpiada2016Judet/Forms/Optiuni.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Olimpiada2016Judet.Models;
+using Olimpiada2016Judet.DataAcces;
 
 namespace Olimpiada2016Judet.Forms
 {
@@ -123,6 +124,7 @@ namespace Olimpiada2016Judet.Forms
         private void tabControl1_Click(object sender, EventArgs e)
         {
             textBox7.Text = textBox4.Text;
+            textBox10.Text = textBox4.Text;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -160,6 +162,118 @@ namespace Olimpiada2016Judet.Forms
             {
                 MessageBox.Show("Comanda esuata!");
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataTable table = DataAcces.SqlDataAcces.GetMeniu();
+
+            List<MeniuModel> mancaruri = new List<MeniuModel>();
+
+            foreach (DataRow item in table.Rows)
+            {
+                MeniuModel men = new MeniuModel
+                {
+                    Id = (int)item["id_produs"],
+                    DenumereaProdus = (string)item["denumire_produs"],
+                    KCal = (int)item["kcal"],
+                    Pret = (int)item["pret"],
+                    Felul = (int)item["felul"]
+                };
+
+                mancaruri.Add(men);
+            }
+
+
+            DataTable newTable = new DataTable();
+            newTable.Columns.Add("IdFelul1");
+            newTable.Columns.Add("IdFelul2");
+            newTable.Columns.Add("IdFelul3");
+            newTable.Columns.Add("Felul1");
+            newTable.Columns.Add("Felul2");
+            newTable.Columns.Add("Felul3");
+            newTable.Columns.Add("Total Kcal");
+            newTable.Columns.Add("Pret total");
+
+            int budget = 0;
+            int kcalMax = 0;
+            int.TryParse(textBox8.Text, out budget);
+            int.TryParse(textBox10.Text, out kcalMax);
+
+            List<MeniuModel> feluri1 = mancaruri.Where(x => x.Felul == 1).ToList();
+            List<MeniuModel> feluri2 = mancaruri.Where(x => x.Felul == 2).ToList();
+            List<MeniuModel> feluri3 = mancaruri.Where(x => x.Felul == 3).ToList();
+
+            (from fel1 in feluri1
+             from fel2 in feluri2
+             from fel3 in feluri3
+             where fel1.Pret + fel2.Pret + fel3.Pret <= budget && fel1.KCal + fel2.KCal + fel3.KCal <= kcalMax
+             select new
+             {
+                 IdFelul1 = fel1.Id,
+                 IdFelul2 = fel2.Id,
+                 IdFelul3 = fel3.Id,
+                 Felul1 = fel1.DenumereaProdus,
+                 Felul2 = fel2.DenumereaProdus,
+                 Felul3 = fel3.DenumereaProdus,
+                 TotalKCal = fel1.KCal + fel2.KCal + fel3.KCal,
+                 PretTotal = fel1.Pret + fel2.Pret + fel3.Pret
+
+             }).ToList().Select(x =>
+                                  {
+                                      DataRow temp = newTable.NewRow();
+                                      temp["IdFelul1"] = x.IdFelul1;
+                                      temp["IdFelul2"] = x.IdFelul2;
+                                      temp["IdFelul3"] = x.IdFelul3;
+                                      temp["Felul1"] = x.Felul1;
+                                      temp["Felul2"] = x.Felul2;
+                                      temp["Felul3"] = x.Felul3;
+                                      temp["Total Kcal"] = x.TotalKCal;
+                                      temp["Pret total"] = x.PretTotal;
+
+                                      return temp;
+
+                                  }).ToList().ForEach(x => newTable.Rows.Add(x));
+
+            dataGridView2.DataSource = newTable;
+
+            dataGridView2.Columns["IdFelul1"].Visible = dataGridView2.Columns["IdFelul2"].Visible = dataGridView2.Columns["IdFelul3"].Visible = false;
+
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.HeaderText = "Alege";
+            btn.Text = "Alege";
+            btn.UseColumnTextForButtonValue = true;
+
+            dataGridView2.Columns.Add(btn);
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+
+            int row = e.RowIndex;
+            int column = e.ColumnIndex;
+
+            DataGridViewCell cell = dgv.Rows[row].Cells[column];
+
+            if (cell is DataGridViewButtonCell)
+            {
+                DataGridViewRow rowSelected = dgv.Rows[row];
+
+                int id1 = int.Parse((string)rowSelected.Cells["IdFelul1"].Value);
+                int id2 = int.Parse((string)rowSelected.Cells["IdFelul2"].Value);
+                int id3 = int.Parse((string)rowSelected.Cells["IdFelul3"].Value);
+                List<int> idProduse = new int[] { id1, id2, id3 }.ToList();
+
+                FinalizareComanda.Finalizare(utilizator.Id, idProduse, new int[] { 1, 1, 1 }.ToList());
+
+                MessageBox.Show("Comanda trimisa!");
+
+                this.Close();
+                Start.GetInsance().Show();
+            }
+
         }
 
 
